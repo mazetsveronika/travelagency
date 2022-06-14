2ca0ab9b4682ddf576522dc001cff55795f127f8
Veronika Mazets <97733877+mazetsveronika@users.noreply.github.com>
2022-06-14 16:17:09 +0300

    Delete TourService.java

--- src/main/java/by/mazets/travelagency/service/TourService.java
+++
@@ -1,8 +0,0 @@
-package by.mazets.travelagency.service;
-
-import by.mazets.travelagency.exception.TravelAgencyServiceException;
-
-public interface TourService {
-
-    void setHotTour(int id, boolean isHot) throws TravelAgencyServiceException;
-}
