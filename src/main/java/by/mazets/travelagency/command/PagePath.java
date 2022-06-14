87c79b87cdc0cf3b20543e1336b0b46069bab5b2
Veronika Mazets <97733877+mazetsveronika@users.noreply.github.com>
2022-06-14 16:05:52 +0300

    Update PagePath.java

--- src/main/java/by/mazets/travelagency/command/PagePath.java
+++ src/main/java/by/mazets/travelagency/command/PagePath.java
@@ -8,11 +8,8 @@ public class PagePath {
     public static final String LOGIN_PAGE = "WEB-INF/jsp/login.jsp";
     public static final String REGISTER_PAGE = "WEB-INF/jsp/register.jsp";
     public static final String USER_MENU_PAGE = "WEB-INF/jsp/user_menu.jsp";
-    public static final String ADMIN_MENU_PAGE = "WEB-INF/jsp/admin_menu.jsp";
     public static final String ERROR_PAGE = "WEB-INF/jsp/error.jsp";
-    public static final String VIEW_ALL_VOUCHERS = "WEB-INF/jsp/view_all_vouchers.jsp";
-    public static final String FINISH_ORDER = "WEB-INF/jsp/finish_order.jsp";
-    public static final String RESULT_REGISTER_PAGE = "WEB-INF/jsp/result_register.jsp";
     public static final String ACCOUNT_PAGE = "WEB-INF/jsp/account.jsp";
     public static final String ERROR_404 = "WEB-INF/jsp/error.jsp" ;
+    //todo
 }
